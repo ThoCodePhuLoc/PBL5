6f6fc8b97219b7d59dd9df778f9b64fd634aebfd
ThoCodePhuLoc <98513828+ThoCodePhuLoc@users.noreply.github.com>
2023-11-09 17:34:52 +0700

    Add files via upload

---
+++ lfwlayanh.py
@@ -0,0 +1,20 @@
+# Import tensorflow dependencies - Functional API
+
+# Import standard dependencies
+import cv2
+import os
+import random
+import numpy as np
+from matplotlib import pyplot as plt
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
+import tensorflow as tf# Setup paths
+POS_PATH = os.path.join('data', 'positive')
+NEG_PATH = os.path.join('data', 'negative')
+ANC_PATH = os.path.join('data', 'anchor')# Make the directories
+
+for directory in os.listdir('lfw'):
+    for file in os.listdir(os.path.join('lfw', directory)):
+        EX_PATH = os.path.join('lfw', directory, file)
+        NEW_PATH = os.path.join(NEG_PATH, file)
+        os.replace(EX_PATH, NEW_PATH)
